c56e703f84566712fc3d7b27b1c7bb6e5b040f05
evdkv <71773269+evdkv@users.noreply.github.com>
2023-06-15 19:17:00 -0400

    Add the package imports

---
+++ funs.R
@@ -0,0 +1,10 @@
+# Load packages
+suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library(ggrepel))
+suppressPackageStartupMessages(library(stringi))
+suppressPackageStartupMessages(library(lubridate))
+suppressPackageStartupMessages(library(sf))
+suppressPackageStartupMessages(library(hms))
+suppressPackageStartupMessages(library(RcppRoll))
+suppressPackageStartupMessages(library(broom))
+
